26cebf22c8361d433264185ef5818abd89bdd477
dff-staff <dff-staff@outlook.com>
2019-03-30 10:09:55 +0530

    commit

--- src/app/components/foryou/foryou.component.ts
+++ src/app/components/foryou/foryou.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class ForyouComponent {
   title = 'Foryou';
+  cards: any[] = [
+    {
+      "id":"1",
+      "p": "You have a current license and many community connections, but have no time for managing the details of real estate transactions;"
+    },
+    {
+      "id":"2",
+      "p": "You have connections who want to purchase property in distant markets;"
+    },
+    {
+      "id":"3",
+      "p": "You have connections who move into the local area with no knowledge of the area;"
+    },
+   
+  ];
+  cards1: any[] = [
+    {
+      "id":"4",   
+      "p": "You have a license in one state but have moved elsewhere; you can still benefit from real estate transactions happening in your sphere."
+    },
+    {
+      "id":"5",
+      "p": "Still have questions? See  our FAQ’s page!"
+    },
+    {
+      "id":"6",
+      "p": " In sollicitudin at libero ac fermentum. Fusce arcu odio, pretium hendrerit enim eget, tempus mollis diam. "
+    },
+    ];
+    
 }
--- src/app/components/whatdo/whatdo.component.ts
+++ src/app/components/whatdo/whatdo.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class WhatdoComponent {
   title = 'Whatdo';
+  cards: any[] = [
+    {
+      "img":"../../../assets/hand.svg",
+      "p": "Maintains a current real estate license."
+    },
+    {
+      "img":"../../../assets/library.svg",
+      "p": "Completes all required Continuing Education requirements to keep license current."
+    },
+    {
+      "img":"../../../assets/test.svg",
+      "p": "Renew license as scheduled according to local requirements."
+    },
+    {
+      "img" :"../../../assets/hand.svg",
+      "p": "Submits referral clients through the RRR portal."
+    },
+   
+  ];
+
+   
 }
